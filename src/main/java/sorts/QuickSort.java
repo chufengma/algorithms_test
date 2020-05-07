421e21ac5ab9c772cf48f97489a407a6742e12ef
yfchu <yfchu@Ctrip.com>
2020-05-07 21:03:33 +0800

    add sort

--- src/main/java/sorts/QuickSort.java
+++ src/main/java/sorts/QuickSort.java
@@ -28,13 +28,15 @@ public class QuickSort {
             while(array[++i] < theOne) if(i == high) break;
             // 从右往左找比theOne小的
             while(array[--j] > theOne) if(j == low) break;
-            // 当任意一边都没找到
+            // 当任意一边都没找到说明已经分割完毕
             if (i >= j) {
                 break;
             }
             // 交换位置，使小的在左边，大的在右边
             SortMain.exchange(array, i, j);
         }
+
+        // 将此次对比的数字交换到"中间位置"，该位置左边都比它小，右边都比它大
         SortMain.exchange(array, low, j);
         return j;
     }
